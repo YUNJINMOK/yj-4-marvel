3ea6c5864da6a0f46f6fddac30e1be1cb512e8fc
YUNJINMOK <aprnaporn114@naver.com>
2024-01-26 11:12:04 +0900

    123

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import MainPage from "./routes/MainPage.jsx";
 import NotFound from "./routes/NotFound.jsx";
+import MainPage from "./routes/MainPage.jsx";
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
