33e6f537303c4e504cc147195bf724e13c7a10b6
YUNJINMOK <aprnaporn114@naver.com>
2024-01-26 12:59:16 +0900

    asd

--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -8,9 +8,17 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { FaAngleLeft } from "react-icons/fa6";
 import { FaChevronRight } from "react-icons/fa";
+import { useMeasure } from "react-use";
 
-const ListItem = ({ item }) => (
-  <div className="w-[194px] h-[340px]">
+const ListItem = ({ item, CARD_WIDTH, CARD_HEIGHT, MARGIN }) => (
+  <div
+    className=""
+    style={{
+      width: CARD_WIDTH,
+      height: CARD_HEIGHT,
+      margin: MARGIN,
+    }}
+  >
     {/* 1 이미지 박스 */}
     <div className="w-full h-[280px]">
       <img
@@ -29,8 +37,40 @@ const ListItem = ({ item }) => (
 
 export default function MainPage() {
   const { data, isLoading } = useQuery(["getComics"], apiGetComics);
+  let lists; // fetch 요청한 배열을 받기 위한 변수
+  if (!isLoading) {
+    lists = data?.data.results;
+  }
   console.log(isLoading, data);
+
+  const CARD_WIDTH = 195;
+  const CARD_HEIGHT = 340;
+  const MARGIN = 8;
+  const CARD_SIZE = CARD_WIDTH + MARGIN;
+
+  const BREAKPOINT = {
+    sm: 640,
+    lg: 1024,
+  };
+
+  const [ref, { width }] = useMeasure();
   const [offset, setOffset] = useState(0);
+
+  const CARD_BUFFER = width > BREAKPOINT.lg ? 3 : width > BREAKPOINT.sm ? 2 : 1;
+
+  const CAN_SHIFT_LEFT = offset < 0;
+
+  const CAN_SHIFT_RIGHT =
+    Math.abs(offset) < CARD_SIZE * (lists?.length - CARD_BUFFER);
+
+  const shifLeft = () => {
+    if (!CAN_SHIFT_LEFT) return;
+    setOffset((pv) => pv + CARD_SIZE);
+  };
+  const shifRight = () => {
+    if (!CAN_SHIFT_RIGHT) return;
+    setOffset((pv) => (pv -= CARD_SIZE));
+  };
 
   return (
     <Layout>
@@ -39,24 +79,46 @@ export default function MainPage() {
 
       {/* 코믹스 섹션 */}
       <TitleImgeBox imgUrl="https://cdn.britannica.com/62/182362-050-BD31B42D/Scarlett-Johansson-Black-Widow-Chris-Hemsworth-Thor.jpg" />
-      <Layout7>
-        <motion.div
-          animate={{
-            x: offset,
-          }}
-          className="w-full flex"
-        >
-          {data?.data?.results.map((item, index) => (
-            <ListItem item={item} key={index} />
-          ))}
-        </motion.div>
-        <button className="absolute left-0 top-[35%] bg-slate-500/50 p-3  pl-2 text-4xl text-white hover:pl-3">
-          <FaAngleLeft />
-        </button>
-        <button className="absolute left-0 top-[35%] bg-slate-500/50 p-3  pl-2 text-4xl text-white hover:pl-3">
-          <FaChevronRight />
-        </button>
-      </Layout7>
+      <section className="w-full flex justify-center">
+        <div ref={ref} className="relative max-w-7xl w-full">
+          <motion.div
+            animate={{
+              x: offset,
+            }}
+            className="  w-full flex"
+          >
+            {lists?.map((item, index) => (
+              <ListItem
+                CAED_WIDTH={CARD_WIDTH}
+                CARD_HEIGHT={CARD_HEIGHT}
+                MARGIN={MARGIN}
+                item={item}
+                key={index}
+              />
+            ))}
+          </motion.div>
+          <motion.button
+            initial={false}
+            animate={{
+              x: CAN_SHIFT_LEFT ? "0%" : "-100%",
+            }}
+            onClick={shifLeft}
+            className="absolute left-0 top-[35%] bg-slate-500/50 duration-100 p-3  pl-2 text-4xl text-white hover:pl-3"
+          >
+            <FaAngleLeft />
+          </motion.button>
+          <motion.button
+            initial={false}
+            animate={{
+              x: CAN_SHIFT_RIGHT ? "0%" : "100%",
+            }}
+            onClick={shifRight}
+            className="absolute right-0 top-[35%] bg-slate-500/50 duration-100 p-3  pl-2 text-4xl text-white hover:pr-3"
+          >
+            <FaChevronRight />
+          </motion.button>
+        </div>
+      </section>
     </Layout>
   );
 }
